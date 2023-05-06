b2ba07b555963229a9b0c0bbffcb257b6fae7b61
zhang bo <cloudszhang@gmail.com>
2023-05-06 14:43:06 +0800

    已经可以获取简单的特征了

--- feature_loader.py
+++ feature_loader.py
@@ -1,4 +1,6 @@
 #encoding=utf-8
+import pandas as pd
+
 from param import Param
 from data_base import DataBase
 import numpy as np
@@ -9,6 +11,7 @@ class FeatureLoader(object):
         self.param = Param()
         self.db = DataBase(self.param)
         self.algorithm = FeatureAlgorithm()
+        self.dump_cnt = 0
         return
 
 
@@ -18,9 +21,19 @@ class FeatureLoader(object):
             one_df = self.db.get_signal_df(one_ins).copy()
             for sn in range(len(one_df)):
                 one_signal = one_df.iloc[sn, :]
-                self.load_one_feature(one_signal)
+                one_feature = self.load_one_feature(one_signal)
+                self.dump_one_feature(one_feature)
 
         return
+
+    def dump_one_feature(self, one_feature):
+        df = pd.DataFrame([one_feature])
+        if self.dump_cnt == 0:
+            df.to_csv('feature.txt', sep = '\t', index = None, mode = 'w')
+        else:
+            df.to_csv('feature.txt', sep = '\t', index = None, header = None, mode = 'a+')
+        self.dump_cnt += 1
+
 
     def get_next_data(self, one_signal):
         next_data = self.db.load_next_data(one_signal.date, one_signal.time, one_signal.id).copy()
@@ -36,10 +49,32 @@ class FeatureLoader(object):
         return curr_ins_data, all_ins_data
     def load_one_feature(self, one_signal):
         features = dict()
+        features['ins'] = one_signal.ins
+        features['date'] = one_signal.date
+        features['time'] = one_signal.time
+        features['id'] = one_signal.id
+        self.load_next_features(features, one_signal)
+        self.load_curr_features(features, one_signal)
+        self.load_prev_features(features, one_signal)
+
+        return features
+
+    def load_next_features(self, features, one_signal):
         curr_ins_next_data, all_ins_next_data = self.get_next_data(one_signal)
         features['next_10_mins_inc'] = self.algorithm.next_10_min_inc(curr_ins_next_data)
         return
 
+    def load_curr_features(self, features, one_signal):
+        prev_high = one_signal.high
+        prev_low = one_signal.low
+        curr = one_signal.curr
+
+        features['curr_inc'] = (curr / prev_low - 1) * 100.0
+        features['curr_dec'] = (curr / prev_high - 1) * 100.0
+        return
+
+    def load_prev_features(self, features, one_signal):
+        return
 
 if __name__ == '__main__':
     feature = FeatureLoader()
