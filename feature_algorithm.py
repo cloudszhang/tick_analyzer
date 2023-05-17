23cee92fc4dd4c51db745973ab0bdb7e210d2a58
zhang bo <cloudszhang@gmail.com>
2023-05-17 11:17:49 +0800

    Ôö¼ÓÁË1¸öÌØÕ÷

--- feature_algorithm.py
+++ feature_algorithm.py
@@ -80,18 +80,21 @@ class FeatureAlgorithm(object):
         if inc_tick is None:
             if dec_tick is not None:
                 start_tick = dec_tick
+                increase = -1.0
             else:
                 return np.nan
         else:
             if dec_tick is None:
                 start_tick = inc_tick
+                increase = 1.0
             else:
                 inc_sn = inc_tick.name
                 dec_sn = dec_tick.name
                 start_tick = inc_tick if inc_sn > dec_sn else dec_tick
+                increase = 1.0 if inc_sn > dec_sn else -1.0
 
         tick_cnt = curr_tick.name - start_tick.name
-        return tick_cnt
+        return tick_cnt, increase
 
     def recent_slope(self, prev_df):
         df = prev_df.copy()
@@ -119,7 +122,7 @@ class FeatureAlgorithm(object):
         ratio = range / min_bid
 
         return ratio
-    
+
     def prev_flat(self, prev_df):
         df = prev_df.copy()
         df = df[df.bid1 > 0.1]
--- feature_loader.py
+++ feature_loader.py
@@ -95,7 +95,7 @@ class FeatureLoader(object):
     def load_prev_features(self, features, one_signal):
         prev_data, all_prev_data = self.get_prev_data(one_signal)
 
-        features['inc_speed'] = self.algorithm.prev_inc_speed(prev_data)
+        features['inc_speed'], features['inc_dir'] = self.algorithm.prev_inc_speed(prev_data)
         features['recent_slope'] = self.algorithm.recent_slope(prev_data)
         features['recent_amp'] = self.algorithm.recent_amp(prev_data)
         features['prev_flat'] = self.algorithm.prev_flat(prev_data)
